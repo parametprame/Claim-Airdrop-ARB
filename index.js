a2d76b8a9577baecec887f0079e4d78cdf0c447a
parametprame <parametprame2@gmail.com>
2023-03-20 16:50:35 +0700

    update

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const ethers = require("ethers");
 require("dotenv").config();
 
 const ClaimTokenABI = require("./ABI/ClaimTokenABI.json");
+const ERC20ABI = require("./ABI/ERC20ABI.json");
 
 async function FastClaim() {
   const PRIVATE_KEY = process.env.PRIVATE_KEY;
   const RPC = process.env.RPC;
   const RPC_ETHEREUM = process.env.RPC_ETHEREUM;
   const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+  const CONTRACT_TOKEN = process.env.CONTRACT_TOKEN;
 
   const provider = new ethers.providers.JsonRpcProvider(RPC);
 
@@ -16,16 +18,27 @@ async function FastClaim() {
 
   const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
   const contract = new ethers.Contract(CONTRACT_ADDRESS, ClaimTokenABI, wallet);
+  const ArbToken = new ethers.Contract(CONTRACT_TOKEN, ERC20ABI, wallet);
+
+  console.log(`Target Wallet Address: ${wallet.address}`);
 
   const blockHasClaimed = await contract.claimPeriodStart();
   const currentBlock = await providerOnEthereum.getBlockNumber();
 
   if (currentBlock >= blockHasClaimed.toString()) {
     await contract.claim();
+    const amount = await ArbToken.balanceOf(wallet.address);
+
+    // Approve Token
+    // TODO: change spender to address is you want
+    await ArbToken.approve("spender", amount);
+
+    // Do Something. swap, etc.
   } else {
     console.log(
       `Not time to claim token. Time To Claim : ${blockHasClaimed.toString()}, Current Time : ${currentBlock}`
     );
+    return;
   }
 }
 
