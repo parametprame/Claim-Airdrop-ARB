b6a619183faa5fd6a400eb57cb09aef854a5abb2
parametprame <parametprame2@gmail.com>
2023-03-21 14:27:03 +0700

    update

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 const ethers = require("ethers");
-
-require("dotenv").config();
-
+const dotenv = require("dotenv");
 const ClaimTokenABI = require("./ABI/ClaimTokenABI.json");
 const ERC20ABI = require("./ABI/ERC20ABI.json");
 
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const RPC = process.env.RPC;
+const RPC_ETHEREUM = process.env.RPC_ETHEREUM;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const CONTRACT_TOKEN = process.env.CONTRACT_TOKEN;
+const RECEIVER_ADDRESS = process.env.RECEIVER_ADDRESS;
+
+const provider = new ethers.providers.JsonRpcProvider(RPC);
+const providerOnEthereum = new ethers.providers.JsonRpcProvider(RPC_ETHEREUM);
+const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+const contract = new ethers.Contract(CONTRACT_ADDRESS, ClaimTokenABI, wallet);
+const ArbToken = new ethers.Contract(CONTRACT_TOKEN, ERC20ABI, wallet);
+
 async function FastClaim() {
-  const PRIVATE_KEY = process.env.PRIVATE_KEY;
-  const RPC = process.env.RPC;
-  const RPC_ETHEREUM = process.env.RPC_ETHEREUM;
-  const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
-  const CONTRACT_TOKEN = process.env.CONTRACT_TOKEN;
-  const RECEIVER_ADDRESS = process.env.RECEIVER_ADDRESS;
-
-  const provider = new ethers.providers.JsonRpcProvider(RPC);
-
-  const providerOnEthereum = new ethers.providers.JsonRpcProvider(RPC_ETHEREUM);
-
-  const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
-  const contract = new ethers.Contract(CONTRACT_ADDRESS, ClaimTokenABI, wallet);
-  const ArbToken = new ethers.Contract(CONTRACT_TOKEN, ERC20ABI, wallet);
-
   const blockHasClaimed = await contract.claimPeriodStart();
   const currentBlock = await providerOnEthereum.getBlockNumber();
 
   if (currentBlock >= blockHasClaimed.toString()) {
-    await contract.claim();
+    const claimTransaction = contract.claim();
+
+    await claimTransaction.wait();
+
     const amount = await ArbToken.balanceOf(wallet.address);
-    console.log(`You recevied : ${parseInt(amount.toString())} $ARB.`);
-    await ArbToken.transfer(RECEIVER_ADDRESS, amount);
+    console.log(
+      `You received : ${parseInt(amount.toString()) / 10 ** 18} $ARB.`
+    );
+
+    const transferTransaction = ArbToken.transfer(RECEIVER_ADDRESS, amount);
+
+    await transferTransaction.wait();
   } else {
     console.log(
       `Not time to claim token. Time To Claim : ${blockHasClaimed.toString()}, Current Time : ${currentBlock}`
@@ -36,4 +42,5 @@ async function FastClaim() {
   }
 }
 
-setInterval(FastClaim, 5000);
+const cron = require("node-cron");
+cron.schedule("*/5 * * * * *", FastClaim);
