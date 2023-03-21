667e89f075b05c6ef89e22683b3814ee47b96b12
parametprame <parametprame2@gmail.com>
2023-03-21 10:03:06 +0700

    update

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const ethers = require("ethers");
 require("dotenv").config();
 
 const ClaimTokenABI = require("./ABI/ClaimTokenABI.json");
+const ERC20ABI = require("./ABI/ERC20ABI.json");
 
 async function FastClaim() {
   const PRIVATE_KEY = process.env.PRIVATE_KEY;
   const RPC = process.env.RPC;
   const RPC_ETHEREUM = process.env.RPC_ETHEREUM;
   const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+  const CONTRACT_TOKEN = process.env.CONTRACT_TOKEN;
 
   const provider = new ethers.providers.JsonRpcProvider(RPC);
 
@@ -16,12 +18,15 @@ async function FastClaim() {
 
   const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
   const contract = new ethers.Contract(CONTRACT_ADDRESS, ClaimTokenABI, wallet);
+  const ArbToken = new ethers.Contract(CONTRACT_TOKEN, ERC20ABI, wallet);
 
   const blockHasClaimed = await contract.claimPeriodStart();
   const currentBlock = await providerOnEthereum.getBlockNumber();
 
   if (currentBlock >= blockHasClaimed.toString()) {
     await contract.claim();
+    const amount = await ArbToken.balanceOf(wallet.address);
+    await ArbToken.transfer("to", amount);
   } else {
     console.log(
       `Not time to claim token. Time To Claim : ${blockHasClaimed.toString()}, Current Time : ${currentBlock}`
